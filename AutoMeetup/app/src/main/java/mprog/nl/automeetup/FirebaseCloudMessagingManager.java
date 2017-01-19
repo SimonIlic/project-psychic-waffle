1b88cfc91d120a1c3e8205996a52cbe38952507e
SimonIlic <simon.ilic@student.uva.nl>
2017-01-19 13:32:41 +0100

    Created FCMManager class

---
+++ AutoMeetup/app/src/main/java/mprog/nl/automeetup/FirebaseCloudMessagingManager.java
@@ -0,0 +1,14 @@
+package mprog.nl.automeetup;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * A manager class that will manage all push notification related bussiness
+ * 
+ *
+ * Created by Simon on 19-1-2017.
+ */
+
+public class FirebaseCloudMessagingManager extends FirebaseInstanceIdService{
+
+}
