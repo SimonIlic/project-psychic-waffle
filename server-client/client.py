4bce2cbc8cc56b2a3acef55e487d0ca9b9bdad56
SimonIlic <simon.ilic@student.uva.nl>
2017-01-20 13:08:37 +0100

    Connected to firebase

--- server-client/client.py
+++ server-client/client.py
@@ -2,17 +2,17 @@
 
 import pyrebase
 
+import os
+
 config = {
   "apiKey": "AIzaSyDqzSSZhlYov3BIxMPCT5l2NrMJYjjPwfo",
   "authDomain": "automeetup.firebaseapp.com",
   "databaseURL": "https://automeetup.firebaseio.com",
-  "storageBucket": "automeetup.appspot.com"
-  "serviceAccount": "C:\Users\simon\Documents\mprog\final-project\secureKey\AutoMeetup-f15490fb4556.json"
-  
+  "storageBucket": "automeetup.appspot.com",
+  "serviceAccount": 'service-accounts\AutoMeetup-f15490fb4556.json'
 }
 
 firebase = pyrebase.initialize_app(config)
 
 # get a database listener
 db = firebase.database()
-
