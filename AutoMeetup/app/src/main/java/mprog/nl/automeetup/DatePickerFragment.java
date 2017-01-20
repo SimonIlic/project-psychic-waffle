e90f05c549398305f16e3677bec70a9178b9fff3
SimonIlic <simon.ilic@student.uva.nl>
2017-01-20 15:10:42 +0100

    created datepickerfragment

---
+++ AutoMeetup/app/src/main/java/mprog/nl/automeetup/DatePickerFragment.java
@@ -0,0 +1,37 @@
+package mprog.nl.automeetup;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+
+/**
+ * Date picker fragment class as per google's prescription:
+ * https://developer.android.com/guide/topics/ui/controls/pickers.html#DatePicker
+ *
+ * Created by Simon on 20-1-2017.
+ */
+
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current date as the default date in the picker
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        // Create a new instance of DatePickerDialog and return it
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        // Do something with the date chosen by the user
+    }
+
+
+}
