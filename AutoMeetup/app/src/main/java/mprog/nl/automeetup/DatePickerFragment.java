1e899f6f3c072e221524b5511f53bb2a43bcaa00
SimonIlic <simon.ilic@student.uva.nl>
2017-01-22 00:21:57 +0100

    worked on dialogs

--- AutoMeetup/app/src/main/java/mprog/nl/automeetup/DatePickerFragment.java
+++ AutoMeetup/app/src/main/java/mprog/nl/automeetup/DatePickerFragment.java
@@ -4,19 +4,47 @@ import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.DatePicker;
 
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Date picker fragment class as per google's prescription:
  * https://developer.android.com/guide/topics/ui/controls/pickers.html#DatePicker
  *
+ * for all interface stuff Xaver Kapeller (stackoverflow) was a HUGE help:
+ * http://stackoverflow.com/questions/24558835/how-can-i-pass-the-date-chosen-in-a-date-picker-to-the-activity-which-contains-t
+ *
  * Created by Simon on 20-1-2017.
  */
 
 public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+    private DatePickerFragmentListener datePickerListener;
+    public interface DatePickerFragmentListener {
+        public void onDateSet(Date date, String tag);
+    }
+
+    public DatePickerFragmentListener getDatePickerListener() {
+        return this.datePickerListener;
+    }
+
+    public void setDatePickerListener(DatePickerFragmentListener listener) {
+        this.datePickerListener = listener;
+    }
+
+    protected void notifyDatePickerListener(Date date, String tag) {
+        if(this.datePickerListener != null) {
+            this.datePickerListener.onDateSet(date, tag);
+        }
+    }
+
+    public static DatePickerFragment newInstance(DatePickerFragmentListener listener) {
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setDatePickerListener(listener);
+        return fragment;
+    }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -31,8 +59,11 @@ public class DatePickerFragment extends DialogFragment implements DatePickerDial
     }
 
     public void onDateSet(DatePicker view, int year, int month, int day) {
-        // Do something with the date chosen by the user
+        Calendar c = Calendar.getInstance();
+        c.set(year, month, day);
+        Date date = c.getTime();
+
+        // Here we call the listener and pass the date back to it.
+        notifyDatePickerListener(date, this.getTag());
     }
-
-
 }
--- AutoMeetup/app/src/main/java/mprog/nl/automeetup/PlanMeetingDialogFragment.java
+++ AutoMeetup/app/src/main/java/mprog/nl/automeetup/PlanMeetingDialogFragment.java
@@ -11,34 +11,47 @@ import android.widget.Button;
 import android.widget.NumberPicker;
 import android.widget.Toast;
 
+import java.util.Date;
+
 /** Plan meeting dialog fragment handles meeting request
  *
  * Created by Simon on 20-1-2017.
  */
 
-public class PlanMeetingDialogFragment extends DialogFragment implements View.OnClickListener{
+public class PlanMeetingDialogFragment extends DialogFragment implements View.OnClickListener,
+        DatePickerFragment.DatePickerFragmentListener{
+    private View layoutView;
+    private Button startDateButton, endDateButton;
+    private Meeting meeting;
+    private NumberPicker numberPicker;
+    String[] nums;
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // generate meeting object
+        meeting = new Meeting();
+
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         // Get the layout inflater and layout view
         LayoutInflater inflater = getActivity().getLayoutInflater();
-        View layoutView = inflater.inflate(R.layout.plan_meeting_dialog_layout, null);
+        layoutView = inflater.inflate(R.layout.plan_meeting_dialog_layout, null);
 
         //
-        setButtonOnClickListeners(layoutView);
-        configureNumberPicker(layoutView);
+        setButtonOnClickListeners();
+        configureNumberPicker();
 
         builder.setTitle("Plan Meeting")
                 .setView(layoutView)
                 .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        // FIRE ZE MISSILES!
+                        // set the selected meeting duration
+                        meeting.setMeetingDuration(Integer.valueOf(nums[numberPicker.getValue()]));
                     }
                 })
                 .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        // User cancelled the dialog
+                        //TODO implement cancel logic
                     }
                 });
         // Create the AlertDialog object and return it
@@ -46,24 +59,24 @@ public class PlanMeetingDialogFragment extends DialogFragment implements View.On
     }
 
     /** configure the number picker for meeting duration **/
-    private void configureNumberPicker(View layoutView) {
-        NumberPicker np = (NumberPicker) layoutView.findViewById(R.id.numberPicker);
-        String[] nums = new String[18];
+    private void configureNumberPicker() {
+        numberPicker = (NumberPicker) layoutView.findViewById(R.id.numberPicker);
+        nums = new String[18];
         for(int i = 2; i - 2 < nums.length; i++) {
             nums[i - 2] = Integer.toString(i * 10);
         }
 
-        np.setMinValue(0);
-        np.setMaxValue(nums.length - 1);
-        np.setWrapSelectorWheel(false);
-        np.setDisplayedValues(nums);
-        np.setValue(3);
+        numberPicker.setMinValue(0);
+        numberPicker.setMaxValue(nums.length - 1);
+        numberPicker.setWrapSelectorWheel(false);
+        numberPicker.setDisplayedValues(nums);
+        numberPicker.setValue(3);
     }
 
     /** Sets the onClickListeners for the buttons in the dialog to **/
-    private void setButtonOnClickListeners(View layoutView) {
-        Button startDateButton = (Button) layoutView.findViewById(R.id.periodStartButton);
-        Button endDateButton = (Button) layoutView.findViewById(R.id.periodEndButton);
+    private void setButtonOnClickListeners() {
+        startDateButton = (Button) layoutView.findViewById(R.id.periodStartButton);
+        endDateButton = (Button) layoutView.findViewById(R.id.periodEndButton);
         startDateButton.setOnClickListener(this);
         endDateButton.setOnClickListener(this);
     }
@@ -73,14 +86,28 @@ public class PlanMeetingDialogFragment extends DialogFragment implements View.On
     public void onClick(View v) {
         if (R.id.periodStartButton == v.getId()) {
             Toast.makeText(getActivity(), "Clicked start button", Toast.LENGTH_SHORT).show();
-            DialogFragment meetingPlannerDialogFragment = new DatePickerFragment();
-            meetingPlannerDialogFragment.show(getFragmentManager(), "DatePicker");
+            DialogFragment meetingPlannerDialogFragment = DatePickerFragment.newInstance(this);
+            meetingPlannerDialogFragment.show(getFragmentManager(), "StartDatePicker");
         }
         else if (R.id.periodEndButton == v.getId()) {
             Toast.makeText(getActivity(), "CLicked end button", Toast.LENGTH_SHORT).show();
-            DialogFragment meetingPlannerDialogFragment = new DatePickerFragment();
-            meetingPlannerDialogFragment.show(getFragmentManager(), "DatePicker");
+            DialogFragment meetingPlannerDialogFragment = DatePickerFragment.newInstance(this);
+            meetingPlannerDialogFragment.show(getFragmentManager(), "EndDatePicker");
         }
 
+    }
+
+    /** Method is called when a date is picked inside the DatePickerFragment
+     * The method will be called with the date from the `DatePicker`. **/
+    @Override
+    public void onDateSet(Date date, String tag) {
+        if (tag.contentEquals("StartDatePicker")){
+            startDateButton.setText(date.toString());
+            meeting.setStartDate(date);
+        }
+        else if (tag.contentEquals("EndDatePicker")) {
+            endDateButton.setText(date.toString());
+            meeting.setEndDate(date);
+        }
     }
 }
