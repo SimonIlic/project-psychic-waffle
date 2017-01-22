6df26e9e88e8c1f93175062698d344f5a0e84801
SimonIlic <simon.ilic@student.uva.nl>
2017-01-22 02:06:59 +0100

    cleanup

--- AutoMeetup/app/src/main/java/mprog/nl/automeetup/MainActivity.java
+++ AutoMeetup/app/src/main/java/mprog/nl/automeetup/MainActivity.java
@@ -6,12 +6,14 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.iid.FirebaseInstanceId;
 
 public class MainActivity extends AppCompatActivity {
 
